e6caaa8a4a582e13047df6c2a262451de0928642
deepak <deepak.t@probyto.com>
2024-02-14 16:18:29 +0530

    third commit

--- test.py
+++ test.py
@@ -1,3 +1,12 @@
+import os
+
 var = "Hello World"
 var_2 = "Deepak"
+def setup_custom_logger_gcp_with_service_id(log_name):
+    os.environ["key_path"] = "./config/ring_central_dev_serviceaccount.json"
+    # Instantiates a client with Service Account
+    # logging_client = logging.Client.from_service_account_json(os.environ["key_path"])
+    # Selects the log to write to
+    # logger = logging_client.logger(log_name)
+    # return logger
 print(var)
